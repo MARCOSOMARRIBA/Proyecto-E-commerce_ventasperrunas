f3b45de2a53f8d68f4016a564c9e25de1558d3fb
Marco Aurelio <sans15cadena@gmail.com>
2026-05-22 23:05:25 -0600

    BACKEND CAMBIOS 13

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,10 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-# 🔥 CORRECCIÓN: Contraseñas directas sin os.environ.get
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False    # Cambia a False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'sans15cadena@gmail.com' 
 EMAIL_HOST_PASSWORD = 'fkni zfmj cplt mfhg' 
 DEFAULT_FROM_EMAIL = 'Ventas Perrunas <sans15cadena@gmail.com>'
